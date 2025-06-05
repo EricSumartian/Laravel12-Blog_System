fd4384b6d21ef4560f915b5117b0a4edd4792bdb
EricSumartian <ericsumartian06@gmail.com>
2025-06-05 21:37:00 +0700

    posts and article

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,20 @@ Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    $posts = [
+        [ 
+            'title'=> 'Judul Artikel 1',
+            'author' => 'Eric Sumartian',
+            'body'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        ],
+        [ 
+            'title'=> 'Judul Artikel 2',
+            'author' => 'John Doe',
+            'body'=> 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        ],
+        ];
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/about', function () {
