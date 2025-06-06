558ac75b2c662d4619c8cb7e639e866158cc24ed
EricSumartian <ericsumartian06@gmail.com>
2025-06-06 23:31:38 +0700

    database

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,43 +9,11 @@ Route::get('/', function () {
 
 
 Route::get('/posts', function () {
-    $posts = [
-        [ 
-            'id' => 1,
-            'title'=> 'Judul Artikel 1',
-            'author' => 'Eric Sumartian',
-            'body'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-        ],
-        [ 
-            'id' => 2,
-            'title'=> 'Judul Artikel 2',
-            'author' => 'John Doe',
-            'body'=> 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        ],
-    ];
+    $posts = Post::all();
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
-Route::get('/posts/{id}', function ($id) {
-        $posts = [
-        [ 
-            'id' => 1,
-            'title'=> 'Judul Artikel 1',
-            'author' => 'Eric Sumartian',
-            'body'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-        ],
-        [ 
-            'id' => 2,
-            'title'=> 'Judul Artikel 2',
-            'author' => 'John Doe',
-            'body'=> 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-        ],
-    ];
-
-    $post = Arr::first($posts, function ($post) use ($id) {
-        return $post['id'] == $id;
-    });
-
+Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
